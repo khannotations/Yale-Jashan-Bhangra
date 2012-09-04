585009b1d9f5264b1f9b5fc50434508ae9c4a810
Rafi Khan <faiaz.a.khan@gmail.edu>
2012-09-03 23:40:31 -0400

    helper fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,23 @@
 class MainController < ApplicationController
 
-  def index
-  end
+
+  helper_method :content_header
+
+  protected
+
+    def content_header selected
+      about = (selected == "about") ? " selected" : ""
+      team = (selected == "team") ? " selected" : ""
+      media = (selected == "media") ? " selected" : ""
+
+      string = "<div class='header'><div class='small-head'>"
+      string += "<li class='img'><a href='/'><img src='/assets/logo_notext.png' /></a></li>"
+      string += "<li class='text"+about+"'><a href='/about'>About</a></li>"
+      string += "<li class='text"+team+"'><a href='/team'>Team</a></li>"
+      string += "<li class='text"+media+"'><a href='/media'>Media</a></li>"
+      string += "<li class='pull-right text' style='padding-top: 10px;'><fb:like href='https://www.facebook.com/pages/Yale-Jashan-Bhangra/190308261019210' send='false' layout='button_count' width='150' show_faces='false'></fb:like></li>"
+      string += "</div></div>"
+      string
+    end
+
 end
