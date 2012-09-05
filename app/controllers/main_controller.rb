c3aa40947e0a14d4617206356e92e85019161cbe
Rafi Khan <faiaz.a.khan@gmail.edu>
2012-09-05 10:15:36 -0400

    header

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -15,7 +15,9 @@ class MainController < ApplicationController
       string += "<li class='text"+about+"'><a href='/about'>About</a></li>"
       string += "<li class='text"+team+"'><a href='/team'>Team</a></li>"
       string += "<li class='text"+media+"'><a href='/media'>Media</a></li>"
-      string += "<li class='pull-right text' style='padding-top: 10px;'><fb:like href='https://www.facebook.com/pages/Yale-Jashan-Bhangra/190308261019210' send='false' layout='button_count' width='150' show_faces='false'></fb:like></li>"
+      string += "<li class='pull-right text'>\
+      <fb:like href='https://www.facebook.com/pages/Yale-Jashan-Bhangra/190308261019210' \
+      send='false' layout='button_count' width='150' show_faces='false'></fb:like></li>"
       string += "</div></div>"
       string
     end
