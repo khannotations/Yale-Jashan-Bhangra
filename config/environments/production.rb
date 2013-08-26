390a98b28528d4d8fe024915a9f725d05d158c6c
Rafi Khan <faiaz.a.khan@gmail.com>
2013-08-26 16:51:31 -0400

    allow iframe headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,12 @@ Bhangra::Application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOW-FROM http://herokuapp.com',
+    'X-XSS-Protection' => '1; mode=block',
+    'X-Content-Type-Options' => 'nosniff'
+  }
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
